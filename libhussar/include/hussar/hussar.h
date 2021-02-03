e311fce47b7045569a43fabffe410d261b937311
Lukas Michel <lukas@michel-nk.de>
2021-02-03 15:04:51 +0100

    add a polyfill atomicAdd for older GPU architectures

--- libhussar/include/hussar/hussar.h
+++ libhussar/include/hussar/hussar.h
@@ -4,6 +4,22 @@
 #if defined(HUSSAR_BUILD_GPU_RENDERER) && defined(__CUDACC__)
 #define HUSSAR_CPU_GPU __host__ __device__
 #define HUSSAR_GPU __device__
+#if __CUDA_ARCH__ < 600
+__device__ double atomicAdd(double* address, double val)
+{
+    unsigned long long int* address_as_ull = (unsigned long long int*)address;
+    unsigned long long int old = *address_as_ull, assumed;
+
+    do {
+        assumed = old;
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+        old = atomicCAS(address_as_ull, assumed,
+            __double_as_longlong(val + __longlong_as_double(assumed)));
+    } while (assumed != old);
+
+    return __longlong_as_double(old);
+}
+#endif
 #else
 #define HUSSAR_CPU_GPU
 #define HUSSAR_GPU
